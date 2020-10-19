42adec06f0e4a6082e7f1da161b0cda11c0c877f
Jorge Parada Fernandez <jwuhon@gmail.com>
2020-10-19 12:14:26 +0200

    ADD: Usuario => Model DbObject

--- PR01/src/codigo/models/Usuario.java
+++ PR01/src/codigo/models/Usuario.java
@@ -1,6 +1,64 @@
 package codigo.models;
 
-public class Usuario {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import codigo.db.DbObject;
+
+public class Usuario extends DbObject {
+	
+	private static final String TABLE = "usuario";
+	private static final ArrayList COLS = getArrayCols();
+	
+	public String login = "";
+	public String pass = "";
+	public int edad = 0;
+	
+
+	private int id = 0;
+	@Override
+	public int getId() {
+		return this.id;
+	}
+
+	@Override
+	public String getTable() { 
+		return TABLE;
+	} 
+	
+	@Override
+	public ArrayList getCols() { 
+		return COLS;
+	}
+	private static ArrayList getArrayCols() {
+		ArrayList list = new ArrayList();
+		list.add("login");
+		list.add("pass"); 
+		list.add("edad"); 
+		return list;
+	}
+
+	@Override
+	public ArrayList getValues() {
+		ArrayList list = new ArrayList();
+		list.add( this.login );
+		list.add( this.pass ); 
+		list.add( this.edad ); 
+		return list;
+	}
+
+	@Override
+	public DbObject parse(ResultSet rs) throws SQLException {
+		Usuario obj = new Usuario();
+		obj.id = rs.getInt("id");
+		
+		obj.login = rs.getString("login");
+		obj.pass  = rs.getString("pass");
+		obj.edad  = rs.getInt("edad");
+		
+		return obj;	
+	}
 	  
 
 }
